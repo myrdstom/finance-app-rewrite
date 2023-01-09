1035b1e4a5b3f421ef88c50c01bd9d43921270cb
myrdstom <nserekopaul@gmail.com>
2023-01-09 05:50:55 +0300

    setup the app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./styles/theme";
+import store from "./redux/combineStore";
+// import Authentication from "./views/auth";
+// import Dashboard from "./pages/dashboard";
+// import Loans from "./pages/loans";
+// import Payments from "./pages/payments";
+// import Clients from "./pages/clients";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Router>
+            <Switch>
+              {/*<Route path="/dashboard" component={Dashboard} />*/}
+              {/*<Route path="/clients" component={Clients} />*/}
+              {/*<Route path="/loans" component={Loans} />*/}
+              {/*<Route path="/payments" component={Payments} />*/}
+              {/*<Route path="/" component={Authentication} />*/}
+            </Switch>
+          </Router>
+        </ThemeProvider>
+      </Provider>
   );
 }
 
