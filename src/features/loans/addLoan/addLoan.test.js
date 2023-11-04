bab7b96b39cd450181167500010c21432cf8842e
myrdstom <nserekopaul@gmail.com>
2023-11-04 19:13:12 +0300

    add loan test

--- src/features/loans/addLoan/addLoan.test.js
+++ src/features/loans/addLoan/addLoan.test.js
@@ -2,23 +2,23 @@ import React from "react";
 import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { RenderWithRouterMatch } from "__tests__/setupTests";
-import { client } from "__tests__/__fixtures__/client";
+import { loans } from "__tests__/__fixtures__/loans";
 import AddLoan from "./addLoan";
 
 describe("Add Client Component", () => {
   test("it should add a loan", async () => {
     const userSetup = userEvent.setup();
-    const { email, address, name, city, number, country } = client;
+    const { client, dateDisbursed, amount, currency, comment } = loans;
     RenderWithRouterMatch(<AddLoan />);
 
     expect(screen.getByRole("button", { name: /Save/i })).toBeEnabled();
     const saveButton = screen.getByTestId("save");
-    // await userSetup.type(screen.getByTestId("client"), name);
-    // await userSetup.type(screen.getByTestId("email"), email);
-    // await userSetup.type(screen.getByTestId("tel-no"), number);
-    // await userSetup.type(screen.getByTestId("address"), address);
-    // await userSetup.type(screen.getByTestId("city"), city);
-    // await userSetup.type(screen.getByTestId("country"), country);
+    await userSetup.type(screen.getByTestId("client"), client);
+    await userSetup.type(screen.getByTestId("selectedDate"), dateDisbursed);
+    await userSetup.type(screen.getByTestId("amount"), amount);
+    await userSetup.type(screen.getByTestId("rate"), "5");
+    await userSetup.type(screen.getByTestId("currency"), currency);
+    await userSetup.type(screen.getByTestId("comment"), comment);
     await userSetup.click(saveButton);
   });
 });
