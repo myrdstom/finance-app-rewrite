88e84cf2c27a80084d8025bdee5c391a023d0bfb
Paul Kayongo <paulkayongo@Pauls-MacBook-Pro.local>
2024-01-10 18:37:34 +0300

    push comment to git

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { theme } from "styles/theme";
 import store from "redux/combineStore";
 import { getTokenData } from "utils/getToken";
 
+// Testing that I can push to git, delete comment
 function App() {
   getTokenData();
   return (
